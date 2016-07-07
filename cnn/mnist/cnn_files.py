f4775900fc66bcb6be75df6b1995f6115d7d01dd
levan <levantsinadze@gmail.com>
2016-07-07 15:55:32 +0400

    improving indentation in modules

--- cnn/mnist/cnn_files.py
+++ cnn/mnist/cnn_files.py
@@ -18,34 +18,34 @@ WEIGHTS_FILE = 'conv_model.ckpt'
 # Files and directories for parameters (trained), training, validation and test
 class training_file(files_and_path_utils):
   
-    def __init__(self):
-      super(self, training_file).__init__('mnist')
-        
-    # Gets current directory of script
-    def get_current(self):
-        
-        current_dir = os.path.dirname(os.path.realpath(__file__))
-        
-        dirs = os.path.split(current_dir)
-        dirs = os.path.split(dirs[0])
-        current_dir = dirs[0]
-        
-        return current_dir
-    
-    # Gets directory for training set and parameters
-    def get_data_directory(self):
-        return self.join_path(self.get_current, self.path_to_cnn_directory, PATH_FOR_TRAINING)
-    
-    # Initializes weights and biases files directory
-    def init_files_directory(self):
-        
-        current_dir = self.join_path(self.get_current, self.path_to_cnn_directory, PATH_FOR_PARAMETERS)
-        
-        if not os.path.exists(current_dir):
-            os.makedirs(current_dir)
-        
-        return current_dir
-    
-    # Gets training data  / parameters directory path
-    def get_or_init_files_path(self):
-        return self.join_path(self.init_files_directory, WEIGHTS_FILE)
+  def __init__(self):
+    super(self, training_file).__init__('mnist')
+      
+  # Gets current directory of script
+  def get_current(self):
+      
+      current_dir = os.path.dirname(os.path.realpath(__file__))
+      
+      dirs = os.path.split(current_dir)
+      dirs = os.path.split(dirs[0])
+      current_dir = dirs[0]
+      
+      return current_dir
+  
+  # Gets directory for training set and parameters
+  def get_data_directory(self):
+      return self.join_path(self.get_current, self.path_to_cnn_directory, PATH_FOR_TRAINING)
+  
+  # Initializes weights and biases files directory
+  def init_files_directory(self):
+      
+      current_dir = self.join_path(self.get_current, self.path_to_cnn_directory, PATH_FOR_PARAMETERS)
+      
+      if not os.path.exists(current_dir):
+          os.makedirs(current_dir)
+      
+      return current_dir
+  
+  # Gets training data  / parameters directory path
+  def get_or_init_files_path(self):
+      return self.join_path(self.init_files_directory, WEIGHTS_FILE)
--- cnn/mnist/cnn_parameters.py
+++ cnn/mnist/cnn_parameters.py
@@ -16,25 +16,24 @@ CNN_DROPOUT = 0.75  # Dropout, probability to keep units
 # Defile weights and biases
 class cnn_weights:
     
-    def __init__(self):
-        
-        # Store layers weight
-        self.weights = {
-            # 5x5 conv, 1 input, 32 outputs
-            'wc1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
-            # 5x5 conv, 32 inputs, 64 outputs
-            'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
-            # fully connected, 7*7*64 inputs, 1024 outputs
-            'wd1': tf.Variable(tf.random_normal([7 * 7 * 64, 1024])),
-            # 1024 inputs, 10 outputs (class prediction)
-            'out': tf.Variable(tf.random_normal([1024, N_CLASSES]))
-        }
-        
-        # Store layers bias
-        self.biases = {
-            'bc1': tf.Variable(tf.random_normal([32])),
-            'bc2': tf.Variable(tf.random_normal([64])),
-            'bd1': tf.Variable(tf.random_normal([1024])),
-            'out': tf.Variable(tf.random_normal([N_CLASSES]))
-        }
-    
+  def __init__(self):
+      
+      # Store layers weight
+      self.weights = {
+          # 5x5 conv, 1 input, 32 outputs
+          'wc1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
+          # 5x5 conv, 32 inputs, 64 outputs
+          'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
+          # fully connected, 7*7*64 inputs, 1024 outputs
+          'wd1': tf.Variable(tf.random_normal([7 * 7 * 64, 1024])),
+          # 1024 inputs, 10 outputs (class prediction)
+          'out': tf.Variable(tf.random_normal([1024, N_CLASSES]))
+      }
+      
+      # Store layers bias
+      self.biases = {
+          'bc1': tf.Variable(tf.random_normal([32])),
+          'bc2': tf.Variable(tf.random_normal([64])),
+          'bd1': tf.Variable(tf.random_normal([1024])),
+          'out': tf.Variable(tf.random_normal([N_CLASSES]))
+      }
