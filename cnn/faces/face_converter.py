67c14bc8fb6415c341a3543f48ad6cc19ba3dfe2
levan-lev <levan-lev@levan-lev>
2017-01-11 00:23:30 +0400

    image utilities improvement

--- cnn/faces/face_converter.py
+++ cnn/faces/face_converter.py
@@ -10,11 +10,12 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
+from skimage import io
 import sys
 
-import dlib
-from skimage import io
 import cv2
+import dlib
 
 
 # Take the image file name from the command line
@@ -44,7 +45,11 @@ for i, face_rect in enumerate(detected_faces):
   # Draw a box around each face we found
   cv2.rectangle(image, (face_rect.left(), face_rect.top()), (face_rect.right(), face_rect.bottom()), (255, 0, 255), 2)
   
-#Write in new file
+  roi = image[face_rect.left():face_rect.top(), face_rect.right():face_rect.bottom()]
+  path = os.path.join(out_image, 'aligned_face_{}.jpg'.format(i))
+  cv2.imwrite(path.format(i), roi)
+  
+# Write in new file
 cv2.imwrite(out_image, image)
           
 # Wait until the user hits <enter> to close the window          
