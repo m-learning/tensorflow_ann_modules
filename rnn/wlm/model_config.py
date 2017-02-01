28d7eb9fd2aaa7b0ac606399a9078333ef47fb5b
levan <levantsinadze@gmail.com>
2017-02-01 19:32:27 +0400

    adding word language model implementation

---
+++ rnn/wlm/model_config.py
@@ -0,0 +1,108 @@
+"""
+Created on Feb 1, 2017
+
+Network model configuration
+
+@author: Levan Tsinadze
+"""
+
+from rnn.wlm import reader
+
+class ModelInput(object):
+  """The input data."""
+
+  def __init__(self, config, data, name=None):
+    
+    self.batch_size = batch_size = config.batch_size
+    self.num_steps = num_steps = config.num_steps
+    self.epoch_size = ((len(data) // batch_size) - 1) // num_steps
+    self.input_data, self.targets = reader.ptb_producer(
+        data, batch_size, num_steps, name=name)
+
+class SmallConfig(object):
+  """Small config."""
+  
+  init_scale = 0.1
+  learning_rate = 1.0
+  max_grad_norm = 5
+  num_layers = 2
+  num_steps = 20
+  hidden_size = 200
+  max_epoch = 4
+  max_max_epoch = 13
+  keep_prob = 1.0
+  lr_decay = 0.5
+  batch_size = 20
+  vocab_size = 10000
+
+
+class MediumConfig(object):
+  """Medium config."""
+  
+  init_scale = 0.05
+  learning_rate = 1.0
+  max_grad_norm = 5
+  num_layers = 2
+  num_steps = 35
+  hidden_size = 650
+  max_epoch = 6
+  max_max_epoch = 39
+  keep_prob = 0.5
+  lr_decay = 0.8
+  batch_size = 20
+  vocab_size = 10000
+
+
+class LargeConfig(object):
+  """Large config."""
+  
+  init_scale = 0.04
+  learning_rate = 1.0
+  max_grad_norm = 10
+  num_layers = 2
+  num_steps = 35
+  hidden_size = 1500
+  max_epoch = 14
+  max_max_epoch = 55
+  keep_prob = 0.35
+  lr_decay = 1 / 1.15
+  batch_size = 20
+  vocab_size = 10000
+
+
+class TestConfig(object):
+  """Tiny config, for testing."""
+  
+  init_scale = 0.1
+  learning_rate = 1.0
+  max_grad_norm = 1
+  num_layers = 1
+  num_steps = 2
+  hidden_size = 2
+  max_epoch = 1
+  max_max_epoch = 1
+  keep_prob = 1.0
+  lr_decay = 0.5
+  batch_size = 20
+  vocab_size = 10000
+
+def get_config(FLAGS):
+  """Gets network configuration
+    Args:
+      FLAGS = training flags
+    Returns:
+      conf = network configuration
+  """
+  
+  if FLAGS.model == "small":
+    conf = SmallConfig()
+  elif FLAGS.model == "medium":
+    conf = MediumConfig()
+  elif FLAGS.model == "large":
+    conf = LargeConfig()
+  elif FLAGS.model == "test":
+    conf = TestConfig()
+  else:
+    raise ValueError("Invalid model: %s", FLAGS.model)
+  
+  return conf
