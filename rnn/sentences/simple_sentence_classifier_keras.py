d3466142e5602c107950af321b0403210b775db3
levan <levantsinadze@gmail.com>
2017-03-17 20:49:10 +0400

    improving training examples

--- rnn/sentences/simple_sentence_classifier_keras.py
+++ rnn/sentences/simple_sentence_classifier_keras.py
@@ -20,7 +20,7 @@ import numpy
 numpy.random.seed(7)
 # load the dataset but only keep the top n words, zero the rest
 top_words = 5000
-(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
+((X_train, y_train), (X_test, y_test)) = imdb.load_data(num_words=top_words)
 # truncate and pad input sequences
 max_review_length = 500
 X_train = sequence.pad_sequences(X_train, maxlen=max_review_length)
