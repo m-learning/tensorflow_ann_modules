6a2d53405f9220c498758bb92dd37cf1a132cbd6
levan <levantsinadze@gmail.com>
2017-03-17 20:45:07 +0400

    improving model loader

--- rnn/sentences/simple_sentence_classifier_keras.py
+++ rnn/sentences/simple_sentence_classifier_keras.py
@@ -30,7 +30,7 @@ embedding_vecor_length = 32
 model = Sequential()
 model.add(Embedding(top_words, embedding_vecor_length, input_length=max_review_length))
 model.add(Dropout(0.2))
-model.add(LSTM(100, dropout_W=0.2, dropout_U=0.2))
+model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
