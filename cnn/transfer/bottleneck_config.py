4221a1812a4b43d288a0145b3571aeee06c89809
levan <levantsinadze@gmail.com>
2017-06-08 03:52:16 +0400

    improving image bottlenecks

--- cnn/transfer/bottleneck_config.py
+++ cnn/transfer/bottleneck_config.py
@@ -84,14 +84,18 @@ def get_or_create_bottleneck_and_path(create_params):
       image_path = dataset.get_image_path(img_path_params)
     else:
       image_path = os.path.join(image_dir, sub_dir, image)
-    if not gfile.Exists(image_path):
-      tf.logging.fatal('File does not exist %s', image_path)
-    image_data = gfile.FastGFile(image_path, 'rb').read()
-    bottleneck_params = (sess, image_data, jpeg_data_tensor, bottleneck_tensor)
-    bottleneck_values = run_bottleneck_on_image(bottleneck_params)
-    bottleneck_string = ','.join(str(x) for x in bottleneck_values)
-    with open(bottleneck_path, 'w') as bottleneck_file:
-      bottleneck_file.write(bottleneck_string)
+    try:
+      if not gfile.Exists(image_path):
+        tf.logging.fatal('File does not exist %s', image_path)
+      image_data = gfile.FastGFile(image_path, 'rb').read()
+      bottleneck_params = (sess, image_data, jpeg_data_tensor, bottleneck_tensor)
+      bottleneck_values = run_bottleneck_on_image(bottleneck_params)
+      bottleneck_string = ','.join(str(x) for x in bottleneck_values)
+      with open(bottleneck_path, 'w') as bottleneck_file:
+        bottleneck_file.write(bottleneck_string)
+    except:
+      print('Not valid image ', image_path)
+      raise
 
   with open(bottleneck_path, 'r') as bottleneck_file:
     bottleneck_string = bottleneck_file.read()
